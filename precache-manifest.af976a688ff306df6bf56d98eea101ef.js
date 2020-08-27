e9daec9defc2a772f05b0dc6d4f1bd103893df11
ricardolicea <ricardo.liceamata@gmail.com>
2020-08-26 21:11:30 -0500

    Updates

---
+++ precache-manifest.af976a688ff306df6bf56d98eea101ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7aa901849e07a09c33528e641232412",
+    "url": "/monsters-roledex/index.html"
+  },
+  {
+    "revision": "7ad04eadfd9688eb3ddd",
+    "url": "/monsters-roledex/static/css/main.4daeb580.chunk.css"
+  },
+  {
+    "revision": "8a536ef43778ba44c872",
+    "url": "/monsters-roledex/static/js/2.7d486076.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-roledex/static/js/2.7d486076.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ad04eadfd9688eb3ddd",
+    "url": "/monsters-roledex/static/js/main.551fc0e7.chunk.js"
+  },
+  {
+    "revision": "e4e1b937cb682fa8c6ff",
+    "url": "/monsters-roledex/static/js/runtime-main.2dbead07.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-roledex/static/media/logo.5d5d9eef.svg"
+  }
+]);
